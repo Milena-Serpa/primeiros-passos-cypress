e3526bf9ec55f2dac86b5ece0f9c635e12ac8ca4
Milena Lima <milenaserpalima@gmail.com>
2025-02-25 08:29:24 +0100

    add gitignore remove node_modules and package-lock.json

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage.js'
 import MenuPage from '../pages/menuPage.js'
 import MyInfoPage from '../pages/myInfoPage.js'
 
+const Chance = require('chance');
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -19,31 +22,9 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
 
-    myinfopage.fillPersonalDetails('Milena', 'Lima', 'Belgian', 'Married')
+    myinfopage.fillPersonalDetails(chance.first(), chance.last(), chance.country({full: true}), 'Married')
     myinfopage.fillEmployeeDetails('01234', '54321', '445678', '2025-02-21')
     myinfopage.saveForm()
   })
 
-  it('login - Fail', () => {
-    loginPage.accessLoginPage()
-    loginPage.loginWithAnyUser(userData.userFail.username, userData.userFail.password)   
-    loginPage.checkAccessInvalid() 
-  })
-
-  it(`menu - teste`, () => {
-    loginPage.accessLoginPage()
-    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
-    menuPage.accessAdmin
-    menuPage.accessPim()
-    menuPage.accessLeave()
-    menuPage.accessTime()
-    menuPage.accessRecruitment()
-    menuPage.accessMyInfo()
-    menuPage.accessPerformance()
-    menuPage.accessDashboard()
-    menuPage.accessDirectory()
-    menuPage.accessMaintenance()
-    menuPage.accessClaim()
-    menuPage.accessBuzz()
-  })
 })
